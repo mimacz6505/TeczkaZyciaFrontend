5d3350e4ec98980fd9aea5aa59dab82c018a0858
mimacz6505 <99917466+mimacz6505@users.noreply.github.com>
2022-03-21 22:12:09 +0100

    Update protractor.conf.js

--- Landing/e2e/protractor.conf.js
+++ Landing/e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://zesp10tt2.herokuapp.com:5000/',
+  baseUrl: 'http://zesp10tt2.herokuapp.com:8080/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
