a84fca76f269d18dde42103373b2069f90bd0f22
mimacz6505 <99917466+mimacz6505@users.noreply.github.com>
2022-03-22 21:51:48 +0100

    Update environment.ts

--- Landing/src/environments/environment.ts
+++ Landing/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'https://zesp10tt2.herokuapp.com:8080'
 };
 
